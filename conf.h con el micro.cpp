eb495a06aae05cc21d575bab6680891482609936
juanalain97 <145404492+juanalain97@users.noreply.github.com>
2023-12-13 20:52:32 -0300

    Add files via upload

---
+++ conf.h con el micro.cpp
@@ -0,0 +1,60 @@
+#ifndef conf_H
+#define conf_H
+
+#include <avr/io.h>
+
+typedef enum {
+    DISPONIBLE,
+    NO_DISPONIBLE
+} EstadoBanco;
+
+void initMicro();
+EstadoBanco estadoDisponible(int contadorPersonas);
+EstadoBanco estadoNoDisponible(int contadorPersonas);
+
+#endif
+
+
+// Definiciones del puerto para el rele
+#define RELE_PORT avr_GPIO_C
+#define RELE_PIN avr_GPIO_PIN_0
+#define RELE avr_GPIOC_OUT_0
+
+// Definiciones de puertos para los pines de sensores
+#define SENSOR_ENTRADA_PORT avr_GPIO_C
+#define SENSOR_ENTRADA_PIN avr_GPIO_PIN_1
+#define LEER_SENSOR_ENTRADA avr_gpio_read_pin(SENSOR_ENTRADA_PORT, SENSOR_ENTRADA_PIN)
+
+#define SENSOR_SALIDA_PORT avr_GPIO_C
+#define SENSOR_SALIDA_PIN avr_GPIO_PIN_2
+#define LEER_SENSOR_SALIDA avr_gpio_read_pin(SENSOR_SALIDA_PORT, SENSOR_SALIDA_PIN)
+
+// defino estados
+void initMicro() {
+    // Configuración de puertos y pines
+    
+}
+
+EstadoBanco estadoDisponible(int contadorPersonas) {
+    if (contadorPersonas >= 1 && contadorPersonas <= 29) {
+        // Encender el rele 
+        avr_gpio_set_pin(RELE_PORT, RELE_PIN);
+        return DISPONIBLE;
+    } else {
+        // Apagar el rele
+        avr_gpio_clear_pin(RELE_PORT, RELE_PIN);
+        return NO_DISPONIBLE;
+    }
+}
+
+EstadoBanco estadoNoDisponible(int contadorPersonas) {
+    if (contadorPersonas >= 30) {
+        // Apagar el rele
+        avr_gpio_clear_pin(RELE_PORT, RELE_PIN);
+        return NO_DISPONIBLE;
+    } else {
+        // Encender el rele
+        avr_gpio_set_pin(RELE_PORT, RELE_PIN);
+        return DISPONIBLE;
+    }
+}
