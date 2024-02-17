b18cc5f340129675f122c80c49445fff0e4adfd9
juanalain97 <145404492+juanalain97@users.noreply.github.com>
2024-02-17 13:19:28 -0300

    Update conf.h con el micro.cpp

--- conf.h con el micro.cpp
+++ conf.h con el micro.cpp
@@ -15,16 +15,11 @@ EstadoBanco estadoNoDisponible(int contadorPersonas);
 #endif
 
 
-// Definiciones del puerto para el rele
-#define RELE_PORT avr_GPIO_C
-#define RELE_PIN avr_GPIO_PIN_0
-#define RELE avr_GPIOC_OUT_0
-
 // Definiciones de puertos para los pines de sensores
 #define SENSOR_ENTRADA_PORT avr_GPIO_C
-#define SENSOR_ENTRADA_PIN avr_GPIO_PIN_1
+#define SENSOR_ENTRADA_PIN avr_GPIO_PIN_A
 #define LEER_SENSOR_ENTRADA avr_gpio_read_pin(SENSOR_ENTRADA_PORT, SENSOR_ENTRADA_PIN)
 
 #define SENSOR_SALIDA_PORT avr_GPIO_C
-#define SENSOR_SALIDA_PIN avr_GPIO_PIN_2
+#define SENSOR_SALIDA_PIN avr_GPIO_PIN_B
 #define LEER_SENSOR_SALIDA avr_gpio_read_pin(SENSOR_SALIDA_PORT, SENSOR_SALIDA_PIN)
