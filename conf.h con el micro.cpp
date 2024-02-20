40ad9504fb0f87717bfecbaf4b8253b8f6551e19
juanalain97 <145404492+juanalain97@users.noreply.github.com>
2024-02-20 14:34:24 -0300

    Update conf.h con el micro.cpp

--- conf.h con el micro.cpp
+++ conf.h con el micro.cpp
@@ -1,24 +1,23 @@
 conf.h:
-#ifndef conf_H
-#define conf_H
+    
+#ifndef CONF_H
+#define CONF_H
 
-// Incluir la API necesaria
-#include "api_gpio.h"
-typedef enum {
-    DISPONIBLE     // pueden entrar y salir personas
-    NO_DISPONIBLE  // no pueden entrar personas, pero si pueden salir
-} EstadoBanco;
+#include "mylib.h"
 
-void initMicro();
-EstadoBanco estadoDisponible(int contadorPersonas);
-EstadoBanco estadoNoDisponible(int contadorPersonas);
+// Definiciones de puertos para el Rele que abre la puerta
+#define RELE_PORT avr_GPIO_C
+#define RELE_PIN  avr_GPIO_PIN_0
+#define RELE  avr_GPIOC_OUT_0
+
+// Definiciones de puertos para los sensores de entrada y salida
+#define SENSOR_PORT avr_GPIO_D
+#define SENSOR_IN_PIN  avr_GPIO_PIN_0
+#define SENSOR_IN  avr_GPIOD_IN_0
+#define SENSOR_OUT_PIN avr_GPIO_PIN_1
+#define SENSOR_OUT avr_GPIOD_IN_1
+
+// Prototipo de la función de inicialización del microcontrolador
+void init_mcu(void);
 
 #endif
-
-
-// Definiciones de puertos para los pines de sensores
-#define SENSOR_ENTRADA_PORT avr_GPIO_C
-#define SENSOR_ENTRADA_PIN avr_GPIO_PIN_A
-
-#define SENSOR_SALIDA_PORT avr_GPIO_C
-#define SENSOR_SALIDA_PIN avr_GPIO_PIN_B
