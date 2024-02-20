67cafad6d0f5108e1187df649576d6314c98ddfa
juanalain97 <145404492+juanalain97@users.noreply.github.com>
2024-02-20 14:38:23 -0300

    Update estados.c

--- estados.c
+++ estados.c
@@ -1,21 +1,53 @@
 #include "conf.h"
 
-estados_t estadoDisponible(int contadorPersonas) {
-    if (contadorPersonas < 30) {
+EstadoBanco estadoDisponible(int *contadorPersonas) {
+    if (*contadorPersonas < 30) {
         // Lógica para el estado disponible
+        if (SENSOR_IN) {
+            (*contadorPersonas)++;
+        } else if (SENSOR_OUT) {
+            if (*contadorPersonas > 0) {
+                (*contadorPersonas)--;
+            }
+        }
+        RELE = 1; // Abrir la puerta
         return DISPONIBLE;
     } else {
         // Lógica para el estado no disponible
+        RELE = 0; // Mantener la puerta cerrada
         return NO_DISPONIBLE;
     }
 }
 
-estados_t estadoNoDisponible(int contadorPersonas) {
-    if (contadorPersonas < 30) {
+EstadoBanco estadoNoDisponible(int *contadorPersonas) {
+    if (*contadorPersonas < 30) {
         // Lógica para el estado disponible
+        if (SENSOR_IN) {
+            (*contadorPersonas)++;
+        } else if (SENSOR_OUT) {
+            if (*contadorPersonas > 0) {
+                (*contadorPersonas)--;
+            }
+        }
+        RELE = 1; // Abrir la puerta
         return DISPONIBLE;
     } else {
         // Lógica para el estado no disponible
+        RELE = 0; // Mantener la puerta cerrada
         return NO_DISPONIBLE;
     }
+}
+
+void init_mcu(void) {
+    GpioInitStructure_AVR rele, sensor;
+   
+    rele.port = RELE_PORT;
+    rele.pines = RELE_PIN;
+    rele.modo = avr_GPIO_mode_Output;
+    init_gpio(rele);
+   
+    sensor.port = SENSOR_PORT ;
+    sensor.pines = SENSOR_IN_PIN  | SENSOR_OUT_PIN  ;
+    sensor.modo = avr_GPIO_mode_Input;
+    init_gpio(sensor);
 }
